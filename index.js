7c78adb2fbf42b668b487294eeefcfe4f697d226
ArshiaP <arshiaputhran08@gmail.com>
2022-08-24 15:29:27 +0530

    small change

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const resolvers = require('./graphql/resolvers')
 
 const { MONGODB } = require('./config.js');
 
+const PORT = process.env.port || 4000;
 
 // In GraphQL, a context is an object shared by all the resolvers of a specific execution. It's useful for keeping data such as authentication info, the current user, database connection, data sources and other things you need for running your business logic.
 
@@ -19,10 +20,13 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB connected");
-    return server.listen({ port: 4000 });
+    return server.listen({ port: PORT });
   })
   .then(res => {
     console.log(`Server running at ${res.url}`)
-  });
+  })
+  .catch(err=>{
+    console.log(err);
+  })
 
 //after login, change authorization header with the new token
